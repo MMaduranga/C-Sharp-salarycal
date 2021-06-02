bba1af4d4cf0d578f9e1acb61bd2c438d3b1ddd9
MMaduranga <m.maduranga433@gmail.com>
2021-06-02 11:24:06 +0530

    first c# project .create a simple salary calculator with visual studio with simple gui

---
+++ Form1.Designer.cs
@@ -0,0 +1,374 @@
+﻿
+namespace EmployeeSalaryCalculator
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.label10 = new System.Windows.Forms.Label();
+			this.textBox6 = new System.Windows.Forms.TextBox();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.label9 = new System.Windows.Forms.Label();
+			this.label11 = new System.Windows.Forms.Label();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.panel3 = new System.Windows.Forms.Panel();
+			this.panel4 = new System.Windows.Forms.Panel();
+			this.panel5 = new System.Windows.Forms.Panel();
+			this.groupBox1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(25, 36);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(135, 23);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Employee No       :";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(25, 79);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(133, 23);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Employee Name :";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(25, 120);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(135, 23);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "Basic Salary         :";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(27, 164);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(131, 23);
+			this.label4.TabIndex = 3;
+			this.label4.Text = "Bonus                  :";
+			// 
+			// textBox1
+			// 
+			this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.textBox1.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox1.Location = new System.Drawing.Point(195, 36);
+			this.textBox1.MaximumSize = new System.Drawing.Size(244, 24);
+			this.textBox1.MinimumSize = new System.Drawing.Size(244, 24);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(244, 24);
+			this.textBox1.TabIndex = 4;
+			// 
+			// textBox2
+			// 
+			this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.textBox2.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox2.Location = new System.Drawing.Point(193, 78);
+			this.textBox2.MaximumSize = new System.Drawing.Size(244, 24);
+			this.textBox2.MinimumSize = new System.Drawing.Size(244, 24);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(244, 24);
+			this.textBox2.TabIndex = 5;
+			// 
+			// textBox3
+			// 
+			this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.textBox3.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox3.Location = new System.Drawing.Point(193, 120);
+			this.textBox3.MaximumSize = new System.Drawing.Size(244, 24);
+			this.textBox3.MinimumSize = new System.Drawing.Size(244, 24);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(244, 24);
+			this.textBox3.TabIndex = 6;
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.BackColor = System.Drawing.Color.White;
+			this.groupBox1.Controls.Add(this.panel3);
+			this.groupBox1.Controls.Add(this.label10);
+			this.groupBox1.Controls.Add(this.panel1);
+			this.groupBox1.Controls.Add(this.textBox6);
+			this.groupBox1.Controls.Add(this.textBox5);
+			this.groupBox1.Controls.Add(this.label7);
+			this.groupBox1.Controls.Add(this.label6);
+			this.groupBox1.Controls.Add(this.label5);
+			this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.groupBox1.Location = new System.Drawing.Point(29, 220);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(410, 190);
+			this.groupBox1.TabIndex = 8;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Over Time Details";
+			// 
+			// label10
+			// 
+			this.label10.AutoSize = true;
+			this.label10.BackColor = System.Drawing.Color.White;
+			this.label10.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label10.Location = new System.Drawing.Point(163, 142);
+			this.label10.MinimumSize = new System.Drawing.Size(228, 32);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(228, 32);
+			this.label10.TabIndex = 14;
+			this.label10.Click += new System.EventHandler(this.label10_Click);
+			// 
+			// textBox6
+			// 
+			this.textBox6.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.textBox6.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox6.Location = new System.Drawing.Point(164, 96);
+			this.textBox6.Name = "textBox6";
+			this.textBox6.Size = new System.Drawing.Size(228, 22);
+			this.textBox6.TabIndex = 11;
+			this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+			// 
+			// textBox5
+			// 
+			this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.textBox5.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox5.Location = new System.Drawing.Point(164, 48);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(227, 22);
+			this.textBox5.TabIndex = 10;
+			this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label7.Location = new System.Drawing.Point(26, 142);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(108, 23);
+			this.label7.TabIndex = 9;
+			this.label7.Text = "OT Amount    :";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label6.Location = new System.Drawing.Point(26, 96);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(109, 23);
+			this.label6.TabIndex = 9;
+			this.label6.Text = "OT Hours       :";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.Location = new System.Drawing.Point(26, 48);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(107, 23);
+			this.label5.TabIndex = 9;
+			this.label5.Text = "OT Rate         :";
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label8.Location = new System.Drawing.Point(25, 453);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(136, 23);
+			this.label8.TabIndex = 9;
+			this.label8.Text = "Net Salary             :";
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.Transparent;
+			this.button1.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(156, 503);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(139, 33);
+			this.button1.TabIndex = 11;
+			this.button1.Text = "Calculate Salary";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button2.Location = new System.Drawing.Point(301, 503);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(138, 33);
+			this.button2.TabIndex = 12;
+			this.button2.Text = "Clear";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.BackColor = System.Drawing.Color.White;
+			this.label9.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label9.Location = new System.Drawing.Point(194, 164);
+			this.label9.MaximumSize = new System.Drawing.Size(244, 24);
+			this.label9.MinimumSize = new System.Drawing.Size(244, 32);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(244, 32);
+			this.label9.TabIndex = 13;
+			this.label9.Click += new System.EventHandler(this.label9_Click);
+			// 
+			// label11
+			// 
+			this.label11.AutoSize = true;
+			this.label11.BackColor = System.Drawing.Color.White;
+			this.label11.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label11.Location = new System.Drawing.Point(188, 453);
+			this.label11.MinimumSize = new System.Drawing.Size(244, 32);
+			this.label11.Name = "label11";
+			this.label11.Size = new System.Drawing.Size(244, 32);
+			this.label11.TabIndex = 14;
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.Black;
+			this.panel1.Location = new System.Drawing.Point(163, 69);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(228, 1);
+			this.panel1.TabIndex = 15;
+			// 
+			// panel2
+			// 
+			this.panel2.BackColor = System.Drawing.Color.Black;
+			this.panel2.Location = new System.Drawing.Point(194, 141);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(244, 1);
+			this.panel2.TabIndex = 16;
+			// 
+			// panel3
+			// 
+			this.panel3.BackColor = System.Drawing.Color.Black;
+			this.panel3.Location = new System.Drawing.Point(163, 117);
+			this.panel3.Name = "panel3";
+			this.panel3.Size = new System.Drawing.Size(228, 1);
+			this.panel3.TabIndex = 16;
+			// 
+			// panel4
+			// 
+			this.panel4.BackColor = System.Drawing.Color.Black;
+			this.panel4.Location = new System.Drawing.Point(195, 100);
+			this.panel4.Name = "panel4";
+			this.panel4.Size = new System.Drawing.Size(244, 1);
+			this.panel4.TabIndex = 17;
+			// 
+			// panel5
+			// 
+			this.panel5.BackColor = System.Drawing.Color.Black;
+			this.panel5.Location = new System.Drawing.Point(195, 57);
+			this.panel5.Name = "panel5";
+			this.panel5.Size = new System.Drawing.Size(244, 1);
+			this.panel5.TabIndex = 18;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.White;
+			this.ClientSize = new System.Drawing.Size(462, 558);
+			this.Controls.Add(this.panel5);
+			this.Controls.Add(this.panel4);
+			this.Controls.Add(this.label11);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.label9);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Employee Salary Calculator";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.TextBox textBox6;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.Label label11;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Panel panel3;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Panel panel4;
+		private System.Windows.Forms.Panel panel5;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmployeeSalaryCalculator
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+			clear();
+		
+		}
+
+		private void label9_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+		
+			if (textBox1.Text.Length != 0)
+			{
+				if(textBox2.Text.Length != 0)
+				{
+					if (textBox3.Text.Length != 0)
+					{
+						if (textBox5.Text.Length != 0)
+						{
+							if (textBox6.Text.Length != 0)
+							{
+								salaryCalculate();
+							}
+							else
+							{
+								showMessage("Please Enter OT Hours");
+							}
+
+						}
+						else
+						{
+							showMessage("Please Enter OT Rate");
+						}
+					}
+					else
+					{
+						showMessage("Please Enter Basic Salary");
+					}
+				}
+				else
+				{
+					showMessage("Please Enter Employee Name");
+				}
+
+			}
+			else {
+				showMessage("Please Enter Employee Number");
+			}
+
+			
+		}
+		private void showMessage(string message) {
+			DialogResult dialogResult = MessageBox.Show(message, "Salary Summary");
+		}
+		private void salaryCalculate() {
+
+			//calculate the bonus amount
+			double basicSalary;
+			if (Double.TryParse(textBox3.Text, out basicSalary))
+			{
+				double bonus = (basicSalary / 50);
+				label9.Text = bonus.ToString();
+
+				//calculate OT amount
+				double OT_Rate;
+				double OT_Hours;
+				if (Double.TryParse(textBox5.Text, out OT_Rate))
+				{
+					if (Double.TryParse(textBox6.Text, out OT_Hours))
+					{
+						double OT_Amount = OT_Rate * OT_Hours;
+						label10.Text = OT_Amount.ToString();
+
+						//calculate net salary
+						double netSalary = basicSalary + OT_Amount + bonus;
+						label11.Text = netSalary.ToString();
+
+						//show message
+						string message = "Employee Number: " + textBox1.Text + "\nEmployee Name: " + textBox2.Text + "\nNet Salary: " + netSalary;
+						showMessage(message);
+					}
+					else
+					{
+						showMessage("Invalid OT Hours");
+					}
+				}
+				else
+				{
+					showMessage("Invalid OT Rate");
+				}
+			}
+			else {
+				showMessage("Invalid Salary type");
+			}
+
+			
+			
+
+			
+
+		}
+
+		private void textBox5_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void textBox6_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label10_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			clear();
+
+
+		}
+		private void clear() {
+			textBox1.Text = "";
+			textBox2.Text = "";
+			textBox3.Text = "";
+			textBox5.Text = "";
+			textBox6.Text = "";
+			label9.Text = "";
+			label10.Text = "";
+			label11.Text = "";
+			
+		}
+	}
+}
